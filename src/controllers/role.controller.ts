422ee89c36e3e40a4ceff16d69edc30f135a9e01
anastasiam <anast.moud@gmail.com>
2026-01-05 17:29:38 +0200

    feat: add role controller for list and create endpoints

---
+++ src/controllers/role.controller.ts
@@ -0,0 +1,21 @@
+import {Request, Response, NextFunction} from "express";
+import * as roleService from "../services/role.service";
+
+export const list = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await roleService.findAllRoles();
+    res.status(200).json(result);
+  } catch(err) {
+    res.status(401).json(err);
+  }
+}
+
+export const create = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    console.log(">>", req.body);
+    const result = await roleService.createRole(req.body);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(401).json(err);
+  }
+}
