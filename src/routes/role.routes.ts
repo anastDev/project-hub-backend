a5ccf40d9350ae3fbc3f2a1fc19c41875aaad129
anastasiam <anast.moud@gmail.com>
2026-01-05 17:27:27 +0200

    feat: add role routes with basic GET and POST endpoints

---
+++ src/routes/role.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import * as roleCtrl from "../controllers/role.controller";
+
+const router = Router();
+
+router.get("/", roleCtrl.list);
+router.post("/", roleCtrl.create)
+
+export default router;
