9baa78e202ec4603575cbf3509bd6253c5608726
anastasiam <anast.moud@gmail.com>
2026-01-05 18:12:13 +0200

    feat: add find users endpoint

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from "express";
+import * as userService from "../services/user.service";
+
+export const list = async(req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await userService.findAllUsers(); 
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(401).json({"Error": err});
+  }
+}
