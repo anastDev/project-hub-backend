0a39174460888c9bb9f8ac8d9db5bb50445a295a
anastasiam <anast.moud@gmail.com>
2026-01-05 17:50:13 +0200

    feat: add update method to role service

--- src/services/role.service.ts
+++ src/services/role.service.ts
@@ -1,10 +1,14 @@
 import Role, { IRole } from "../models/role.model";
 
-export const findAllRoles = async() => {
+export const findAllRoles = async () => {
   return Role.find().lean();
-}
+};
 
-export const createRole = async(payload: Partial<IRole>) => {
+export const createRole = async (payload: Partial<IRole>) => {
   const result = new Role(payload);
   return result.save();
-}
+};
+
+export const updateRole = async (id: string, payload: Partial<IRole>) => {
+  return Role.findByIdAndUpdate(id, payload, { new: true });
+};
