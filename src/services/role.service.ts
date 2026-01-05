36439da0a4433734f487efa07114276248c40068
anastasiam <anast.moud@gmail.com>
2026-01-05 17:30:12 +0200

    feat: add role service with list and create methods

---
+++ src/services/role.service.ts
@@ -0,0 +1,10 @@
+import Role, { IRole } from "../models/role.model";
+
+export const findAllRoles = async() => {
+  return Role.find().lean();
+}
+
+export const createRole = async(payload: Partial<IRole>) => {
+  const result = new Role(payload);
+  return result.save();
+}
