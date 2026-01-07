88f81449f3c422972772a9a1895db27871240959
anastasiam <anast.moud@gmail.com>
2026-01-07 15:07:30 +0200

    feat: add auth login endpoint

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import * as authService from "../services/auth.service";
+
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { username, password } = req.body;
+    const result = await authService.login(username, password);
+    if (!result) {
+      return res.status(401).json({ message: "Invalid Credentials" });
+    }
+    res.status(201).json({ token: result.token, user: result.user });
+  } catch (err) {
+    res.status(401).json({ message: "Error in login", error: err });
+  }
+};
