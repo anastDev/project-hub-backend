29c3f0fa07c13c9494f432a2ecf96bce892ea9a1
anastasiam <anast.moud@gmail.com>
2026-01-07 19:38:22 +0200

    feat: add swagger config

---
+++ src/swagger.ts
@@ -0,0 +1,44 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import SwaggerUi from "swagger-ui-express";
+import mongooseToSwagger from "mongoose-to-swagger";
+import { Express } from "express";
+import Role from "./models/role.model";
+import User from "./models/user.model";
+
+const options: swaggerJSDoc.Options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Project Hub Backend API",
+      version: "1.0.0",
+      description: "Api for users written in TypeScript",
+    },
+    server: [
+      {
+        url: "http://localhost:3000/api",
+        description: "Local Server",
+      },
+    ],
+    components: {
+      securitySchemas: {
+        bearerAuth: {
+          type: "http",
+          schema: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+      schemas: {
+        User: mongooseToSwagger(User),
+        Role: mongooseToSwagger(Role),
+      },
+    },
+    security: [{ bearerAuth: [] }],
+  },
+  apis: [".src/routes/*.ts"],
+};
+
+export const swaggerSpec = swaggerJSDoc(options);
+
+export const setupSwagger = (app: Express) => {
+  app.use("/api/docs", SwaggerUi.serve, SwaggerUi.setup(swaggerSpec));
+};
