26f0a8a075619b66429664254ac4a74c4af73d14
anastasiam <anast.moud@gmail.com>
2026-01-12 23:12:32 +0200

    feat(swagger): add Swagger documentation for login endpoint

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -5,6 +5,36 @@ import { loginSchema } from "../validators/auth.validator";
 
 const router = Router();
 
+/**
+ * @openapi
+ *   /login:
+ *    post:
+ *      summary: Login user
+ *      tags: 
+ *        - Auth
+ *      
+ *      requestBody:
+ *        required: true
+ *        content: 
+ *          application/json:
+ *            schema:
+ *              $ref: "#/components/schemas/LoginResponse"
+ * 
+ *      responses:
+ *        "200": 
+ *          description: Login successful
+ *          content: 
+ *            application/json:
+ *              schema:
+ *                $ref: "#/components/schemas/LoginResponse"
+ * 
+ *        "400":
+ *          $ref: "#/components/responses/BadRequestError"
+ * 
+ *        "401":
+ *          $ref: "#/components/responses/UnauthorizedError"
+ * 
+ */
 router.post("/login", validate(loginSchema), authCtrl.login);
 
 export default router;
