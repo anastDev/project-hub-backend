36cc8104779901e7887a061e21fa26b938cd477d
anastasiam <anast.moud@gmail.com>
2026-01-12 16:21:59 +0200

    feat(docs): add Swagger setup and configuration

--- src/swagger.ts
+++ src/swagger.ts
@@ -34,7 +34,7 @@ const options: swaggerJSDoc.Options = {
     },
     security: [{ bearerAuth: [] }],
   },
-  apis: [".src/routes/*.ts"],
+  apis: ["./src/routes/*.ts"],
 };
 
 export const swaggerSpec = swaggerJSDoc(options);
