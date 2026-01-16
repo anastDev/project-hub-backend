63008c89fad2dd727e0abb62eb14ee96fb5295c8
anastasiam <anast.moud@gmail.com>
2026-01-16 16:47:18 +0200

    tests(users): add admin-only tests for get and delete

--- src/tests/user.integration.test.ts
+++ src/tests/user.integration.test.ts
@@ -1,7 +1,6 @@
 import { TestServer } from "./testSetup";
 import userRoutes from "../routes/user.routes";
 import User from "../models/user.model";
-import Role from "../models/role.model";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
@@ -13,31 +12,61 @@ const server = new TestServer();
 server.app.use("/users", userRoutes);
 
 let token: string;
+let adminToken: string;
 let userId: string;
+let adminUserId: string;
 let testUserData: any;
 
 beforeAll(async () => {
   await server.start();
 
-  const hash = await bcrypt.hash("admin1234", 10);
-  const user = await User.create({
-    username: "admin",
+  const hash = await bcrypt.hash("user1234", 10);
+
+  const regularUser = await User.create({
+    username: "user",
     password: hash,
     firstname: "testUser",
     lastname: "testUser",
     email: "testUser@email.com",
   });
 
-  userId = user._id.toString();
-  testUserData = user.toObject();
+  userId = regularUser._id.toString();
+  testUserData = regularUser.toObject();
 
   const payload = {
     id: userId,
-    username: user.username,
-    email: user.email,
-    roles: user.roles,
+    username: regularUser.username,
+    firstname: regularUser.firstname,
+    lastname: regularUser.lastname,
+    email: regularUser.email,
   };
   token = jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
+
+  const hashedAdmin = await bcrypt.hash("admin1234", 10);
+
+  const adminUser = await User.create({
+    username: "admin",
+    password: hashedAdmin,
+    firstname: "testAdmin",
+    lastname: "testAdmin",
+    email: "adminUser@email.com",
+    roles: [
+      {
+        role: "ADMIN",
+        active: true,
+      },
+    ],
+  });
+
+  adminUserId = adminUser._id.toString();
+
+  const adminPayload = {
+    id: adminUserId,
+    username: adminUser.username,
+    email: adminUser.email,
+    roles: adminUser.roles,
+  };
+  adminToken = jwt.sign(adminPayload, JWT_SECRET, { expiresIn: "1h" });
 });
 
 afterAll(async () => {
@@ -251,5 +280,111 @@ describe("User API Tests PUT Requests (non-admin)", () => {
       .get(`/users/${nonValidId}`)
       .set("Authorization", `Bearer ${token}`);
     expect(res.status).toBe(400);
+  });
+});
+
+describe("User API Tests GET Requests (Admin only)", () => {
+  test("GET /users -> returns a list of users (Admin only restriction)", async () => {
+    const res = await server.request
+      .get("/users")
+      .set("Authorization", `Bearer ${adminToken}`);
+
+    expect(res.status).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
+  });
+
+  test("GET /users -> invalid token", async () => {
+    const res = await server.request
+      .get("/users")
+      .set("Authorization", `Bearer not a valid token`);
+
+    expect(res.status).toBe(401);
+  });
+
+  test("GET /users -> expired token", async () => {
+    const expiredPayload = {
+      username: "testuser",
+      password: "123456",
+      email: "testUser@email.com",
+    };
+
+    const expiredToken = jwt.sign(expiredPayload, JWT_SECRET, {
+      expiresIn: "-1h",
+    });
+
+    const res = await server.request
+      .get("/users")
+      .set("Authorization", `Bearer ${expiredToken}`);
+
+    expect(res.status).toBe(401);
+  });
+
+  test("GET /users -> user isn't an admin", async () => {
+    const res = await server.request
+      .get("/users")
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(res.status).toBe(403);
+  });
+});
+
+describe("User API Tests DELETE Requests (Admin only)", async () => {
+  test("DELETE /users/{id} -> returns a user by ID", async () => {
+    const res = await server.request
+      .delete(`/users/${adminUserId}`)
+      .set("Authorization", `Bearer ${adminToken}`);
+    expect(res.status).toBe(204);
+  });
+
+  test("DELETE /users/{id} -> doesn't have a valid objectId", async () => {
+    const nonValidId = "507f1f77bcf86";
+
+    const res = await server.request
+      .delete(`/users/${nonValidId}`)
+      .set("Authorization", `Bearer ${adminToken}`);
+    expect(res.status).toBe(400);
+  });
+
+  test("DELETE /users/{id} -> invalid token", async () => {
+    const res = await server.request
+      .delete(`/users/${adminUserId}`)
+      .set("Authorization", `Bearer not a valid token`);
+
+    expect(res.status).toBe(401);
+  });
+
+  test("DELETE /users/{id} -> expired token", async () => {
+    const expiredPayload = {
+      username: "testuser",
+      password: "123456",
+      email: "testUser@email.com",
+    };
+
+    const expiredToken = jwt.sign(expiredPayload, JWT_SECRET, {
+      expiresIn: "-1h",
+    });
+
+    const res = await server.request
+      .delete(`/users/${adminUserId}`)
+      .set("Authorization", `Bearer ${expiredToken}`);
+
+    expect(res.status).toBe(401);
+  });
+
+  test("DELETE /users/{id} -> user isn't an admin", async () => {
+    const res = await server.request
+      .delete(`/users/${userId}`)
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(res.status).toBe(403);
+  });
+
+  test("DELETE /users/{id} -> doesn't find the user", async () => {
+    const nonExistentId = "507f1f77bcf86cd799439011";
+
+    const res = await server.request
+      .delete(`/users/${nonExistentId}`)
+      .set("Authorization", `Bearer ${adminToken}`);
+    expect(res.status).toBe(404);
   });
 });
