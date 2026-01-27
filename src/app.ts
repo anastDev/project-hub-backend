a17f274bb1887a8545fcd6037e4e5842d158967f
anastasiam <anast.moud@gmail.com>
2026-01-27 17:06:45 +0100

    fix(cors): added OPTIONS and PATCH methods to allow update

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,7 @@ app.use(
   cors({
     origin: allowedOrigins,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS"],
     allowedHeaders: ["Content-type", "Authorization"],
   }),
 );
