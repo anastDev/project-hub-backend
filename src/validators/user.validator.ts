a18da5646cfa65fab625701b70ab86fcf7bf45a5
anastasiam <anast.moud@gmail.com>
2026-01-27 19:28:15 +0100

    fix(userSchema): flatten the createUserSchema to fix mismatch in frontend and backend

--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -3,19 +3,6 @@ import { extendZodWithOpenApi } from "@asteasolutions/zod-to-openapi";
 
 extendZodWithOpenApi(z);
 
-export const phoneSchema = z.object({
-  phoneType: z.enum(['phone', 'home', 'work']).optional().or(z.literal("")),
-  phoneNumber: z.string(),
-});
-
-export const addressSchema = z.object({
-  area: z.string().optional(),
-  street: z.string().optional(),
-  number: z.string().optional(),
-  po: z.string().optional(),
-  municipality: z.string().optional(),
-});
-
 export const createUserSchema = z.object({
   username: z
     .string()
@@ -28,8 +15,13 @@ export const createUserSchema = z.object({
   email: z
     .email("Invalid email address")
     .min(1, { error: "Email is required" }),
-  address: addressSchema.optional(),
-  phone: z.array(phoneSchema).optional(),
+  area: z.string().optional(),
+  street: z.string().optional(),
+  number: z.string().optional(),
+  po: z.string().optional(),
+  municipality: z.string().optional(),
+  phoneType: z.enum(["phone", "home", "work"]).optional().or(z.literal("")),
+  phoneNumber: z.string(),
 });
 
 export const updateUserSchema = createUserSchema.partial();
