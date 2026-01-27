ff1a25a7ff68574646321efec78ff6b4873db8b2
anastasiam <anast.moud@gmail.com>
2026-01-27 20:02:55 +0100

    fix(user.validator):  align updateUserSchema to frontend schema

--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -24,4 +24,24 @@ export const createUserSchema = z.object({
   phoneNumber: z.string().optional(),
 });
 
-export const updateUserSchema = createUserSchema.partial();
+export const updateUserSchema = z.object({
+   username: z.string()
+        .min(3, "Username must be at least 3 characters")
+        .optional(),
+
+    email: z.email('Invalid email address').min(1, {error: "Email is required"}).optional(),
+
+    password: z.string()
+        .min(5, "Password must be at least 5 characters")
+        .optional()
+        .or(z.literal('')), 
+    firstname: z.string().optional(),
+    lastname: z.string().optional(),
+    area: z.string().optional(),
+    street: z.string().optional(),
+    number: z.string().optional(),
+    po: z.string().optional(),
+    municipality: z.string().optional(),
+    phoneType: z.enum(['phone', 'home', 'work']).optional().or(z.literal("")),
+    phoneNumber: z.string().optional(), 
+});
